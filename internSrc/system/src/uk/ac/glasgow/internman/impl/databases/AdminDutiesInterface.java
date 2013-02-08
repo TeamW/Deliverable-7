a92a4bfd8910e3d0653b779181a20c30718d4f41
Gordon Reid <gordon.reid1992@hotmail.co.uk>
2013-02-08 17:33:29 +0000

    Updates admin duties interface based on d6

--- internSrc/system/src/uk/ac/glasgow/internman/impl/databases/AdminDutiesInterface.java
+++ internSrc/system/src/uk/ac/glasgow/internman/impl/databases/AdminDutiesInterface.java
@@ -3,15 +3,14 @@ package uk.ac.glasgow.internman.impl.databases;
 import uk.ac.glasgow.internman.*;
 
 public interface AdminDutiesInterface {
-	
+
 	/* Renamed from addCompany */
-	public boolean addEmployer(String employerName, String employerEmail);
-	
+	public boolean addEmployer(String employerName, String employerEmail, String password);
+
 	/* Renamed from getCompany */
-	public Employer getEmployer();
-	
-	public Student getStudent(String matric);
-	
-	/* What inputs do we want? */
-	public void updateStudent();
+	public Employer getEmployer(String employerName);
+
+	public Student getStudent(String guid);
+
+	public void updateStudent(Student student);
 }
