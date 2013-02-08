3b16e63167a029c0e536bfe90d120cfad40caddd
Gordon Reid <gordon.reid1992@hotmail.co.uk>
2013-02-08 17:29:17 +0000

    Add interface information for databases component
    @iamjamesgallagher is the authenticate interface to your liking?

--- internSrc/system/src/uk/ac/glasgow/internman/impl/databases/AdminDutiesInterface.java
+++ internSrc/system/src/uk/ac/glasgow/internman/impl/databases/AdminDutiesInterface.java
@@ -1,5 +1,17 @@
 package uk.ac.glasgow.internman.impl.databases;
 
-public interface AdminDutiesInterface {
+import uk.ac.glasgow.internman.*;
 
+public interface AdminDutiesInterface {
+	
+	/* Renamed from addCompany */
+	public boolean addEmployer(String employerName, String employerEmail);
+	
+	/* Renamed from getCompany */
+	public Employer getEmployer();
+	
+	public Student getStudent(String matric);
+	
+	/* What inputs do we want? */
+	public void updateStudent();
 }
--- internSrc/system/src/uk/ac/glasgow/internman/impl/databases/AuthenticateInterface.java
+++ internSrc/system/src/uk/ac/glasgow/internman/impl/databases/AuthenticateInterface.java
@@ -2,4 +2,9 @@ package uk.ac.glasgow.internman.impl.databases;
 
 public interface AuthenticateInterface {
 
+	/* Suitable inputs James? */
+	public boolean login(String username, String password);
+	
+	/* Do we need any other methods here? */
+	
 }
