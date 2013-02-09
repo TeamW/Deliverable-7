2f19b28971e41252ea53eccffbbc11733bcc170b
Gordon Reid <gordon.reid1992@hotmail.co.uk>
2013-02-09 13:46:52 +0000

    Adds Visitor implementation

--- internSrc/system/src/uk/ac/glasgow/internman/impl/VisitorImpl.java
+++ internSrc/system/src/uk/ac/glasgow/internman/impl/VisitorImpl.java
@@ -3,15 +3,16 @@ package uk.ac.glasgow.internman.impl;
 import uk.ac.glasgow.internman.Visitor;
 
 public class VisitorImpl implements Visitor {
+	
+	private String name;
 
-	public VisitorImpl() {
-		// TODO Auto-generated constructor stub
+	public VisitorImpl(String name) {
+		this.name = name;
 	}
 
 	@Override
 	public String getName() {
-		// TODO Auto-generated method stub
-		return null;
+		return name;
 	}
 
 }
