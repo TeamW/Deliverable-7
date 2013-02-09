5f7327dd5fcb06c5ef5c39af996c51c5aaad23fc
ryaanwells <ryaanwells@googlemail.com>
2013-02-09 11:34:54 +0000

    Started Admin

--- internSrc/system/src/uk/ac/glasgow/internman/impl/admin/AdminInterface.java
+++ internSrc/system/src/uk/ac/glasgow/internman/impl/admin/AdminInterface.java
@@ -1,5 +1,9 @@
 package uk.ac.glasgow.internman.impl.admin;
 
+import uk.ac.glasgow.internman.Advertisement;
+import uk.ac.glasgow.internman.Student;
+import uk.ac.glasgow.internman.UoGGrade;
+
 /**
  * Called "Blank" in current System Architecture interface. Will be changed to
  * generic "Admin Interface"
@@ -8,5 +12,20 @@ package uk.ac.glasgow.internman.impl.admin;
  * 
  */
 public interface AdminInterface {
+	
+	void publishAdvertisement(int advertisementIndex, String comment);
+	
+	boolean registerNewEmployer(String name, String email);
+	
+	void approveOffer(String matriculation);
+	
+	void assignAcademicVisitor(String matriculation, String visitorName);
+	
+	Advertisement createNewAdvertisement(String applicationDetails);
+	
+	Student selectStudent(String matriculation);
+	
+	void recordVisitAssessment(String matriculation, UoGGrade grade, String description);
+	
 
 }
