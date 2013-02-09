a13686cacd454e881b130e9c2e3394a56f5e1ec9
Gordon Reid <gordon.reid1992@hotmail.co.uk>
2013-02-09 13:09:19 +0000

    Login works for generic user.

--- internSrc/system/src/uk/ac/glasgow/internman/impl/databases/UserDatabase.java
+++ internSrc/system/src/uk/ac/glasgow/internman/impl/databases/UserDatabase.java
@@ -8,8 +8,7 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.HashMap;
 
-import uk.ac.glasgow.internman.Employer;
-import uk.ac.glasgow.internman.Student;
+import uk.ac.glasgow.internman.*;
 import uk.ac.glasgow.internman.users.User;
 
 public class UserDatabase implements AdminDutiesInterface,
@@ -17,7 +16,7 @@ public class UserDatabase implements AdminDutiesInterface,
 
 	private boolean databaseLoaded;
 	private static final String databaseLocation = System
-			.getProperty("user.dir") + "users.database";
+			.getProperty("user.dir") + "/users.database";
 	private File userDatabase;
 	private HashMap<String, User> users;
 
@@ -82,8 +81,29 @@ public class UserDatabase implements AdminDutiesInterface,
 				return false;
 			}
 		} else {
+			System.out.println("Having to create database, okay on first run.");
 			users = new HashMap<String, User>();
-			saveToDatabase();
+			users.put("test", new User("", "", "test", "letmein"));
+			ObjectOutputStream oos = null;
+			try {
+				userDatabase = new File(databaseLocation);
+				FileOutputStream fos = new FileOutputStream(userDatabase);
+				oos = new ObjectOutputStream(fos);
+			} catch (Exception e) {
+				e.printStackTrace();
+				return false;
+			}
+			try {
+				oos.writeObject(users);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			try {
+				oos.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+				return false;
+			}
 		}
 		if (input != null) {
 			try {
@@ -118,6 +138,14 @@ public class UserDatabase implements AdminDutiesInterface,
 			e.printStackTrace();
 			return;
 		}
+	}
+
+	public static final void main(String[] args) {
+		UserDatabase db = new UserDatabase();
+		System.out.println("Logged in: " + db.login("test", "letmein"));
+		System.out.println("Logged in: " + db.login("test", "don'tletmein"));
+		System.out.println("Logged in: " + db.login("idon'texist", "letmein"));
+
 	}
 
 }
