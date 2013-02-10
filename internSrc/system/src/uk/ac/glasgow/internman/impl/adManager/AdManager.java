0b036dc844398481a69b1f0330605ac7d8c0aac9
Gordon Reid <gordon.reid1992@hotmail.co.uk>
2013-02-10 22:35:06 +0000

    Upload stub implementation for James and Kris

---
+++ internSrc/system/src/uk/ac/glasgow/internman/impl/adManager/AdManager.java
@@ -0,0 +1,43 @@
+package uk.ac.glasgow.internman.impl.adManager;
+
+import java.util.Map;
+
+import uk.ac.glasgow.internman.Advertisement;
+
+public class AdManager implements AdManagerInterface {
+
+	public AdManager() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public Advertisement createNewAdvertisement(String applicationDetails) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Advertisement selectAdvertisement(Integer index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void reviseAdvertisement(Integer index, Advertisement revise) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Map<Integer, Advertisement> getAdvertisements() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void publishAdvertisement(Integer adIndex, String comment) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ internSrc/system/src/uk/ac/glasgow/internman/impl/login/LoginImpl.java
@@ -0,0 +1,21 @@
+package uk.ac.glasgow.internman.impl.login;
+
+public class LoginImpl implements Login {
+
+	public LoginImpl() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public boolean verifyUser(String id, String password) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean userLoggedIn(String id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
