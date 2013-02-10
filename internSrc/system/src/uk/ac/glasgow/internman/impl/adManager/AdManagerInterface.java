b16e525561c9c94b21c8c349d15499fe31864712
Gordon Reid <gordon.reid1992@hotmail.co.uk>
2013-02-10 14:12:01 +0000

    Adds AdManager Interface for Kris based on D6

--- internSrc/system/src/uk/ac/glasgow/internman/impl/adManager/AdManagerInterface.java
+++ internSrc/system/src/uk/ac/glasgow/internman/impl/adManager/AdManagerInterface.java
@@ -1,10 +1,23 @@
 package uk.ac.glasgow.internman.impl.adManager;
 
+import uk.ac.glasgow.internman.*;
+import java.util.Map;
+
 /**
  * Called Interact in System Architecture
  * @author 1002536r
  *
  */
 public interface AdManagerInterface {
+	
+	public Advertisement createNewAdvertisement(String applicationDetails);
+	
+	public Advertisement selectAdvertisement(Integer index);
+	
+	public void reviseAdvertisement(Integer index, Advertisement revise);
+	
+	public Map<Integer, Advertisement> getAdvertisements();
+	
+	public void publishAdvertisement(Integer adIndex, String comment);
 
 }
