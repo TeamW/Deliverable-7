af04081d5c620700a2238f913836d06ef6ed5557
Gordon Reid <gordon.reid1992@hotmail.co.uk>
2013-02-11 18:23:45 +0000

    Quick tidy up and implementation of odd things I spoke to Rose about

--- internSrc/system/src/uk/ac/glasgow/internman/impl/Session.java
+++ internSrc/system/src/uk/ac/glasgow/internman/impl/Session.java
@@ -3,95 +3,86 @@ package uk.ac.glasgow.internman.impl;
 import java.util.Date;
 import java.util.Map;
 
-import uk.ac.glasgow.clui.SystemCommand;
-import uk.ac.glasgow.clui.SystemDialogue;
-import uk.ac.glasgow.internman.Advertisement;
-import uk.ac.glasgow.internman.CourseCoordinator;
-import uk.ac.glasgow.internman.Employer;
-import uk.ac.glasgow.internman.InternMan;
-import uk.ac.glasgow.internman.Role;
-import uk.ac.glasgow.internman.Student;
-import uk.ac.glasgow.internman.UoGGrade;
+import uk.ac.glasgow.clui.*;
+import uk.ac.glasgow.internman.*;
 import uk.ac.glasgow.internman.users.User;
 import uk.ac.glasgow.internman.ui.*;
 import uk.ac.glasgow.internman.impl.adManager.AdManager;
 import uk.ac.glasgow.internman.impl.admin.Admin;
-import uk.ac.glasgow.internman.impl.login.LoginImpl;;
+import uk.ac.glasgow.internman.impl.login.LoginImpl;
 
+;
 
 /**
- * Essentially Session(?)
+ * Session
  * 
  * @author 1002536r
- *
+ * 
  */
 public class Session implements InternMan {
 	private LoginImpl login;
 	private User currentUser = null;
 	private AdManager adManager;
 	private Admin admin;
-	
+
 	public Session() {
 		login = new LoginImpl();
 		adManager = new AdManager();
 		admin = Admin.getInstance();
 	}
-	
-	public static void main(String[] args){
-		InternMan facade = new Session();
-		
-		SystemDialogue dialogue = new SystemDialogue(System.in, System.out, System.err);
 
-		InternManCLUI userInterface = new InternManCLUI(facade,dialogue);
+	public static void main(String[] args) {
+		InternMan facade = new Session();
+		SystemDialogue dialogue = new SystemDialogue(System.in, System.out,
+				System.err);
+		InternManCLUI userInterface = new InternManCLUI(facade, dialogue);
 		addCommands(facade, dialogue, userInterface);
 		userInterface.run();
 	}
-	
-	private static void addCommands(InternMan facade, SystemDialogue dialogue, InternManCLUI userInterface)
-	{
-		SystemCommand<InternMan> showAdvertisementSummary
-		=  new ViewAdvertisementSummaryCommand(facade,dialogue);
-	
-		userInterface.addSystemCommand(
-			"ShowAdvertisementSummary", showAdvertisementSummary);
 
-		userInterface.addSystemCommand(
-	    "Login", new LoginCommand(facade,dialogue));
+	private static void addCommands(InternMan facade, SystemDialogue dialogue,
+			InternManCLUI userInterface) {
+		SystemCommand<InternMan> showAdvertisementSummary = new ViewAdvertisementSummaryCommand(
+				facade, dialogue);
 
-		userInterface.addSystemCommand(
-	    "RegisterEmployer", new RegisterEmployerCommand(facade,dialogue));
-	
-	 	userInterface.addSystemCommand(
-	 		"SubmitAdvertisement", new SubmitAdvertisementCommand(facade,dialogue));
+		userInterface.addSystemCommand("ShowAdvertisementSummary",
+				showAdvertisementSummary);
 
-	 	userInterface.addSystemCommand(
-	 		"PublishAdvertisement", new PublishAdvertisementCommand(facade,dialogue));
+		userInterface.addSystemCommand("Login", new LoginCommand(facade,
+				dialogue));
 
-	
-	 	userInterface.addSystemCommand(
-	 		"ViewAdvertisementDetail", new ViewAdvertisementDetailCommand(facade,dialogue));
+		userInterface.addSystemCommand("RegisterEmployer",
+				new RegisterEmployerCommand(facade, dialogue));
 
-	 	userInterface.addSystemCommand(
-	 		"ViewStudentDetail", new ViewStudentDetailCommand(facade,dialogue));
-	
-	 	userInterface.addSystemCommand(
-	 		"AcceptOffer", new AcceptOfferCommand(facade,dialogue));
-	
-	 	userInterface.addSystemCommand(
-	 		"ApproveOffer", new ApproveOfferCommand(facade,dialogue));
-	
-	 	userInterface.addSystemCommand(
-	 		"AssignVisitor", new AssignAcademicVisitorCommand(facade,dialogue));
-	
-	 	userInterface.addSystemCommand(
-	 		"RecordVisitAssessment", new RecordVisitAssessmentCommand(facade,dialogue));
+		userInterface.addSystemCommand("SubmitAdvertisement",
+				new SubmitAdvertisementCommand(facade, dialogue));
+
+		userInterface.addSystemCommand("PublishAdvertisement",
+				new PublishAdvertisementCommand(facade, dialogue));
+
+		userInterface.addSystemCommand("ViewAdvertisementDetail",
+				new ViewAdvertisementDetailCommand(facade, dialogue));
+
+		userInterface.addSystemCommand("ViewStudentDetail",
+				new ViewStudentDetailCommand(facade, dialogue));
+
+		userInterface.addSystemCommand("AcceptOffer", new AcceptOfferCommand(
+				facade, dialogue));
+
+		userInterface.addSystemCommand("ApproveOffer", new ApproveOfferCommand(
+				facade, dialogue));
+
+		userInterface.addSystemCommand("AssignVisitor",
+				new AssignAcademicVisitorCommand(facade, dialogue));
+
+		userInterface.addSystemCommand("RecordVisitAssessment",
+				new RecordVisitAssessmentCommand(facade, dialogue));
 	}
 
 	@Override
 	public boolean login(String userName, String password) {
-		//if the user is already logged in just return true
 		currentUser = login.verifyUser(userName, password);
-		if(currentUser == null)
+		if (currentUser == null)
 			return false;
 		return true;
 	}
@@ -108,26 +99,25 @@ public class Session implements InternMan {
 
 	@Override
 	public Map<String, Student> getStudents() {
-		//no method for this yet?
-		if(currentUser instanceof CourseCoordinator)
+		if (currentUser instanceof CourseCoordinator)
 			return admin.getStudents();
 		return null;
 	}
 
 	@Override
 	public Employer registerNewEmployer(String name, String emailAddress) {
-		//DOES NOT CHECK THAT EMPLOYER ALREADY REGISTERED
-		if(currentUser instanceof CourseCoordinator || 
-		   currentUser instanceof Employer)
+		if (currentUser instanceof CourseCoordinator
+				|| currentUser instanceof Employer)
 			return admin.registerNewEmployer(name, emailAddress);
 		return null;
 	}
 
 	@Override
 	public Advertisement createNewAdvertisement(String applicationDetails) {
-		if(currentUser instanceof CourseCoordinator || 
-		   currentUser instanceof Employer)
-			return adManager.createNewAdvertisement((Employer) currentUser, applicationDetails);
+		if (currentUser instanceof CourseCoordinator
+				|| currentUser instanceof Employer)
+			return adManager.createNewAdvertisement((Employer) currentUser,
+					applicationDetails);
 		return null;
 	}
 
@@ -138,8 +128,8 @@ public class Session implements InternMan {
 
 	@Override
 	public Role selectRole(Integer advertisementIndex, Integer roleIndex) {
-		//no idea
-		return null;
+		Advertisement temp = adManager.selectAdvertisement(advertisementIndex);
+		return temp != null ? temp.getRoles().get(roleIndex) : null;
 	}
 
 	@Override
@@ -155,7 +145,10 @@ public class Session implements InternMan {
 	@Override
 	public void notifyAcceptedOffer(Role role, String managerName,
 			String managerEmail) {
-		//needs to be implemented in one of the interfaces.
+		System.out
+				.println("Notified course coordinator of acceptance of role: "
+						+ role.getTitle() + " offered by " + managerName + " "
+						+ managerEmail);
 	}
 
 	@Override
@@ -176,7 +169,9 @@ public class Session implements InternMan {
 
 	@Override
 	public Employer getCurrentEmployer() {
-		//Method makes no sense, remnant of earlier code? Ignored.
+		if (currentUser instanceof Employer) {
+			return (Employer) currentUser;
+		}
 		return null;
 	}
 
