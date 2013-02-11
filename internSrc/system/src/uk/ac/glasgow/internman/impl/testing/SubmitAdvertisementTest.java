67a2c3b5ccd1176d439a1964eaac5fa127b641db
David Selkirk <david_selkirk@hotmail.co.uk>
2013-02-11 22:35:01 +0000

    Stub test classes added

---
+++ internSrc/system/src/uk/ac/glasgow/internman/impl/testing/SubmitAdvertisementTest.java
@@ -0,0 +1,14 @@
+package uk.ac.glasgow.internman.impl.testing;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SubmitAdvertisementTest {
+	
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
