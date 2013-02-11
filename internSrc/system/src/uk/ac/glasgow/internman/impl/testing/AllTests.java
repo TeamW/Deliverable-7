bae2f4e96beec402da549104c58aa27e1edb66da
Gordon Reid <gordon.reid1992@hotmail.co.uk>
2013-02-11 22:50:54 +0000

    Add class to run every test

---
+++ internSrc/system/src/uk/ac/glasgow/internman/impl/testing/AllTests.java
@@ -0,0 +1,11 @@
+package uk.ac.glasgow.internman.impl.testing;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ ApproveAcceptedOffer.class, LoginTest.class,
+		NotifyAcceptedOffer.class, PublishAdvertisementTest.class,
+		SubmitAdvertisementTest.class })
+public class AllTests {
+}
