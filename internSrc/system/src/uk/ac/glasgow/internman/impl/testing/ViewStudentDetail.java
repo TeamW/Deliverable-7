18c915addd779e468e1cef5b69dc5d85591c949b
ryaanwells <ryaanwells@googlemail.com>
2013-02-12 22:40:17 +0000

    View student as CC

--- internSrc/system/src/uk/ac/glasgow/internman/impl/testing/ViewStudentDetail.java
+++ internSrc/system/src/uk/ac/glasgow/internman/impl/testing/ViewStudentDetail.java
@@ -2,14 +2,46 @@ package uk.ac.glasgow.internman.impl.testing;
 
 import static org.junit.Assert.*;
 
+import java.util.Date;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import uk.ac.glasgow.internman.Employer;
+import uk.ac.glasgow.internman.Role;
+import uk.ac.glasgow.internman.Student;
+import uk.ac.glasgow.internman.Student.Programme;
+import uk.ac.glasgow.internman.impl.EmployerImpl;
+import uk.ac.glasgow.internman.impl.InternshipImpl;
+import uk.ac.glasgow.internman.impl.RoleImpl;
+import uk.ac.glasgow.internman.impl.Session;
+import uk.ac.glasgow.internman.impl.StudentImpl;
+import uk.ac.glasgow.internman.impl.databases.UserDatabase;
+
 public class ViewStudentDetail {
+
+	UserDatabase ub;
+	Session s;
+	Student student;
+	Employer e;
+	Role r;
 
 	@Before
 	public void setUp() throws Exception {
+		s = new Session();
+		ub = UserDatabase.getInstance();
+		ub.changeCourseCoordinator("TestCC", "letmein");
+		student = new StudentImpl("Gordon", "Reid", "example@example.com",
+				"1002536r", Programme.SE);
+		ub.updateStudent(student);
+		e = new EmployerImpl("someEmployer", "example@example.com", "letmein");
+		ub.addEmployer(e);
+		r = new RoleImpl("TeaMaker", "MyHouse", new Date(), new Date(),
+				"awesome job", 123912.12);
+		student.setInternship(new InternshipImpl("john",
+				"john@someEmployer.com", e, r));
+		student.getInternship().accept();
 	}
 
 	@After
@@ -17,8 +49,12 @@ public class ViewStudentDetail {
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void ViewStudentDetailAsCC() {
+		s.login("TestCC", "letmein");
+		Student stud = s.selectStudent("1002536r");
+		assertEquals(stud.getEmail() + stud.getForename() + stud.getSurname()
+				+ stud.getUsername() + stud.getMatriculation(),
+				"example@example.comGordonReid1002536r1002536r");
 	}
 
 }
