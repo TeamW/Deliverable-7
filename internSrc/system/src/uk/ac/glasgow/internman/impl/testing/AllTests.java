a84ca91b7fb5ad7671dce8660f4f26f8633a7b1c
Gordon Reid <gordon.reid1992@hotmail.co.uk>
2013-02-12 20:32:47 +0000

    Uploads remaining dummy test cases

--- internSrc/system/src/uk/ac/glasgow/internman/impl/testing/AllTests.java
+++ internSrc/system/src/uk/ac/glasgow/internman/impl/testing/AllTests.java
@@ -6,6 +6,8 @@ import org.junit.runners.Suite;
 @RunWith(Suite.class)
 @Suite.SuiteClasses({ ApproveAcceptedOffer.class, LoginTest.class,
 		NotifyAcceptedOffer.class, PublishAdvertisementTest.class,
-		SubmitAdvertisementTest.class })
+		SubmitAdvertisementTest.class, ViewStudentDetail.class,
+		ViewAdvertisementSummary.class, RegisterEmployer.class,
+		ViewAdvertisementDetail.class })
 public class AllTests {
 }
