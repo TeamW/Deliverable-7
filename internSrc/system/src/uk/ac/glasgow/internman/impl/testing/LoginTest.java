e31388b34d3862914fbd0565039d13f8984d9160
James Gallagher <jamesgall90@gmail.com>
2013-02-13 20:28:45 +0000

    Java doc comments added for login test.java

--- internSrc/system/src/uk/ac/glasgow/internman/impl/testing/LoginTest.java
+++ internSrc/system/src/uk/ac/glasgow/internman/impl/testing/LoginTest.java
@@ -2,6 +2,7 @@ package uk.ac.glasgow.internman.impl.testing;
 
 import static org.junit.Assert.*;
 
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -29,46 +30,98 @@ public class LoginTest {
 		ub.addEmployer(e);
 	}
 
+	
+	/**
+	**Test case for Correct student Login - should Return true 
+	*	*/
+	
 	@Test
 	public void studentLoginCorrect() {
 		assertEquals(s.login("1002536r", "letmein"), true);
 	}
 
+	/**
+	**Test case 1 for InCorrect student Login -
+	*Password is Incorrect
+	* should Return false 
+	*	*/
+	
 	@Test
 	public void studentLoginIncorrectPassword() {
 		assertEquals(s.login("1002536r", "notmypassword"), false);
 	}
 
+	/**
+	**Test case 2 for InCorrect student Login
+	*Username is incorrect
+	* - should Return false 
+	*	*/
+	
 	@Test
 	public void studentLoginIncorrectUsername() {
 		assertEquals(s.login("000000a", "notmypassword"), false);
 	}
+	
+	
+	/**
+	**Test case for Correct employer Login -
+	** should Return true
+	*	*/
 
 	@Test
 	public void employerLoginCorrect() {
 		assertEquals(s.login("someEmployer", "letmein"), true);
 	}
 
+	/**
+	**Test case 1 for InCorrect employer Login -
+	*Password is Incorrect
+	* should Return false 
+	*	*/
+	
 	@Test
 	public void employerLoginIncorrectPassword() {
 		assertEquals(s.login("someEmployer", "notmypassword"), false);
 	}
+	
+	/**
+	**Test case 2 for InCorrect employer Login -
+	*User is Incorrect
+	* should Return false 
+	*	*/
 
 	@Test
 	public void employerLoginIncorrectUsername() {
 		assertEquals(s.login("notAnEmployer", "notmypassword"), false);
 	}
 
+	/**
+	**Test case for Correct Course Coordinator Login -
+	** should Return true
+	*	*/
+	
 	@Test
 	public void ccLoginCorrect() {
 		assertEquals(s.login("TestCC", "letmein"), true);
 	}
 
+	/**
+	**Test case for 1 InCorrect Course Coordinator Login -
+	*password is incorrect
+	** should Return false
+	*	*/
+	
 	@Test
 	public void ccLoginIncorrectPassword() {
 		assertEquals(s.login("TestCC", "notmypassword"), false);
 	}
 
+	/**
+	**Test case for 2 InCorrect Course Coordinator Login -
+	*Username is incorrect
+	** should Return false
+	*	*/
+	
 	@Test
 	public void ccLoginIncorrectUsername() {
 		assertEquals(s.login("NotTheCC", "notmypassword"), false);
