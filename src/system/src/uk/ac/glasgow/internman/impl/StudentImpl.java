974b1be3ee57f3b5fe97b978a5f9ee0689118e0a
David Selkirk <david_selkirk@hotmail.co.uk>
2013-02-24 23:54:59 +0000

    More changes
    Errors stemming from getInternship return type change still in the rest
    of the code.

--- src/system/src/uk/ac/glasgow/internman/impl/StudentImpl.java
+++ src/system/src/uk/ac/glasgow/internman/impl/StudentImpl.java
@@ -1,6 +1,7 @@
 package uk.ac.glasgow.internman.impl;
 
 import java.io.Serializable;
+import java.util.Date;
 import java.util.ArrayList;
 
 import uk.ac.glasgow.internman.Internship;
@@ -22,6 +23,7 @@ public class StudentImpl extends User implements Student, Serializable {
 	private String matric;
 	private ArrayList<Internship> internships;
 	private Programme programme;
+	private boolean sufficientInternships = false;
 
 	public StudentImpl(String forename, String surname, String email,
 			String matric, Programme programme) {
@@ -44,12 +46,36 @@ public class StudentImpl extends User implements Student, Serializable {
 	public void setInternship(Internship i) {
 		// need to check that internships don't overlap.
 		// overlap if ( start1 < end2 and start2 < end1 )
-		for(Internship internship : internships){
-			if(i.getRole().getStart().before(internship.getRole().getEnd())
+		for( Internship internship : internships){
+			if( i.getRole().getStart().before(internship.getRole().getEnd())
 				&& internship.getRole().getStart().before(i.getRole().getEnd()))
 				return; // overlap, return
 		}
 		internships.add(i);
+		
+		// check if student now has sufficient internships
+		calcSufficientInternships();
+	}
+	
+	// check if student now has sufficient internships
+	private void calcSufficientInternships(){
+		Date start = internships.get(0).getRole().getStart();
+		Date end = internships.get(0).getRole().getEnd();
+		for( Internship internship : internships){
+			if( internship.getRole().getStart().before(start))
+				start = internship.getRole().getStart();
+			if( internship.getRole().getEnd().after(end))
+				end = internship.getRole().getEnd();
+		}
+		
+		// get the difference in milliseconds, subtract and convert to days
+		final long DAY_IN_MILLIS = 1000 * 60 * 60 * 24;
+		int diffInDays = (int) ((end.getTime() - start.getTime())/ DAY_IN_MILLIS );
+		
+		// if the number of days in internships is greater than or equal to the desired 
+		// time of internships (at the moment 12 weeks) change the flag to true
+		if(diffInDays >= 7*12)
+			sufficientInternships = true;
 	}
 
 	@Override
@@ -75,6 +101,10 @@ public class StudentImpl extends User implements Student, Serializable {
 	@Override
 	public Programme getProgramme() {
 		return programme;
+	}
+	
+	public Boolean getSufficientInternships() {
+		return sufficientInternships;
 	}
 
 }
